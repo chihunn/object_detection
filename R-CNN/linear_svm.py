e6df68a150fe17414720468ae1b80258e3021bff
chihunn <sch804@pukyong.ac.kr>
2023-08-22 22:16:47 +0900

    Add pascal_voc dataset

---
+++ R-CNN/linear_svm.py
@@ -0,0 +1,13 @@
+import time
+import copy
+import os
+import random
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import DataLoader
+import torchvision.transforms as transforms
+from torchvision.models import alexnet
+
+
